a34ee6122f833e2dbd1e9b8064e216f8c22859ff
dliebermankt8e <40901967+dliebermankt8e@users.noreply.github.com>
2022-08-21 18:18:31 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,34 @@
-let myImage: Image = null
-myImage.showImage(0)
+input.onButtonPressed(Button.A, function () {
+    RunApp = 1
+})
+function Spin () {
+    motobit.enable(MotorPower.On)
+    motobit.invert(Motor.Left, true)
+    motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 50)
+    motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 50)
+}
+input.onButtonPressed(Button.B, function () {
+    RunApp = 0
+    motobit.enable(MotorPower.Off)
+})
+let SpinEnable = 0
+let RunApp = 0
+motobit.enable(MotorPower.Off)
+basic.showLeds(`
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
+    # . . . #
+    `)
+music.playMelody("E B C5 A B G A F ", 360)
 basic.forever(function () {
-	
+    if (RunApp) {
+        SpinEnable = pins.digitalReadPin(DigitalPin.P0)
+        if (SpinEnable) {
+            Spin()
+        } else {
+            motobit.enable(MotorPower.Off)
+        }
+    }
 })
